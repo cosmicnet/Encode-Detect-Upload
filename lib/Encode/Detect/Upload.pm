3ea3ac348bf81b5061d35d0414cce733b56cd80f
cosmicnet <webmaster@cosmicperl.com>
2013-11-03 14:13:45 +0000

    First load of working version

---
+++ lib/Encode/Detect/Upload.pm
@@ -0,0 +1,111 @@
+package Encode::Detect::Upload;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Encode::Detect::Upload - The great new Encode::Detect::Upload!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Encode::Detect::Upload;
+
+    my $foo = Encode::Detect::Upload->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Lyle Hopkins, C<< <webmaster at cosmicperl.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-encode-detect-upload at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Encode-Detect-Upload>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Encode::Detect::Upload
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Encode-Detect-Upload>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Encode-Detect-Upload>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Encode-Detect-Upload>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Encode-Detect-Upload/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Lyle Hopkins.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Encode::Detect::Upload
